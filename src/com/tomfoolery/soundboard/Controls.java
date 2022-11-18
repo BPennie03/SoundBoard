04685a3dc0b65fd882c0fe699a9fba2c6c386a50
Reese Hatfield <hatfield.69@wright.edu>
2022-11-18 09:54:37 -0500

    Updated error messages

--- src/com/tomfoolery/soundboard/Controls.java
+++ src/com/tomfoolery/soundboard/Controls.java
@@ -43,6 +43,7 @@ public class Controls extends JPanel {
         });
         this.add(btn);
 
+
     }
 
     public void initRefresh(){
--- src/com/tomfoolery/soundboard/Utils/Utils.java
+++ src/com/tomfoolery/soundboard/Utils/Utils.java
@@ -8,18 +8,18 @@ import java.util.Objects;
 
 public class Utils {
     public static OS getOS() throws OperatingSystemNotFoundException {
-        String os = System.getProperty("os.name");
-        if(os.toLowerCase().contains("mac")){
+        String os = System.getProperty("os.name").toLowerCase();
+        if(os.contains("mac")){
             return OS.OSX;
         }
-        else if(os.toLowerCase().contains("windows")){
+        else if(os.contains("windows")){
             return OS.Windows;
         }
-        else if(os.toLowerCase().contains("nix") || os.toLowerCase().contains("nux") || os.toLowerCase().contains("aix")){
+        else if(os.contains("nix") || os.contains("nux") || os.contains("aix")){
             return OS.Unix;
         }
         else{
-            throw new OperatingSystemNotFoundException("The application operating system is not supported!");
+            throw new OperatingSystemNotFoundException("This operating system is not supported!");
         }
 
     }
@@ -29,5 +29,7 @@ public class Utils {
         int numFiles = Objects.requireNonNull(dir.list()).length;
         return ((int) Math.sqrt(numFiles)) + 1;
     }
+
+
 
 }
